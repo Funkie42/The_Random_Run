11c327b33e3b118a75635ed5f836905ee6655836
Tom Müller <Tom Mller>
2015-11-12 19:20:12 +0100

    erste Version des Hindernis modules

---
+++ Hindernis.py
@@ -0,0 +1,16 @@
+import pygame
+from pygame.locals import *
+
+class platform():
+	def __init__(self, coordinaten_liste, background):
+		self.coordinaten_liste = coordinaten_liste #liste von Koordinaten
+		self.background = background
+		self.moment_right = 0
+		
+	def selfblit(self, source):
+		for i in range(len(self.coordinaten_liste)):
+			surf = pygame.Surface((self.coordinaten_liste[i][0], self.coordinaten_liste[i][1]))
+			source.blit(surf, (self.moment_right, 600 - self.coordinaten_liste[i][1]))
+			rect = surf.get_rect()
+			print(self.moment_right)
+			self.moment_right = self.moment_right +  rect.right
