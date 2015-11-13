808b56be3b3d4fcd9ce3f83d1df4173ad46336ae
Tom Müller <Tom Mller>
2015-11-13 18:49:07 +0100

    Update Hindernis mit blit

--- Hindernis.py
+++ Hindernis.py
@@ -10,7 +10,12 @@ class platform():
 	def selfblit(self, source):
 		for i in range(len(self.coordinaten_liste)):
 			surf = pygame.Surface((self.coordinaten_liste[i][0], self.coordinaten_liste[i][1]))
+			for y in range(0, surf.get_height(), self.background.get_height()):
+				for x in range(0, surf.get_width(), self.background.get_width()):
+					surf.blit(self.background, (x, y))
 			source.blit(surf, (self.moment_right, 600 - self.coordinaten_liste[i][1]))
 			rect = surf.get_rect()
 			print(self.moment_right)
 			self.moment_right = self.moment_right +  rect.right
+	
+			
