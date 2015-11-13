80698cf5fcecbf7ebe1eab0e2b479abd2d7196c9
Tom Müller <Tom Mller>
2015-11-13 19:19:25 +0100

    Update Hindernis mit platform classe

--- Hindernis.py
+++ Hindernis.py
@@ -1,7 +1,7 @@
 import pygame
 from pygame.locals import *
 
-class platform():
+class ground():
 	def __init__(self, coordinaten_liste, background):
 		self.coordinaten_liste = coordinaten_liste #liste von Koordinaten
 		self.background = background
@@ -17,5 +17,19 @@ class platform():
 			rect = surf.get_rect()
 			print(self.moment_right)
 			self.moment_right = self.moment_right +  rect.right
-	
+			
+
+class platform(pygame.Surface):
+	def __init__(self, width, height, top, left , background):
+		super().__init__((width, height))
+		self.top = top
+		self.left = left
+		self.background = background
+		
+	def selfblit(self, source):
+		for y in range(0, self.get_height(), self.background.get_height()):
+				for x in range(0, self.get_width(), self.background.get_width()):
+					self.blit(self.background, (x, y))
+		source.blit(self, (self.top, self.left))
+
 			
