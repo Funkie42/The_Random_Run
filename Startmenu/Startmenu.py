e4a1edf746a5d3208594118c7518db02b120a21d
Marius Funk <funkm@cip.ifi.lmu.de>
2015-11-28 19:38:04 +0000

    Man kann jetzt durchs menü sich klicken, aber noch sieht man nicht was was ist^^

--- Startmenu/Startmenu.py
+++ Startmenu/Startmenu.py
@@ -13,68 +13,63 @@ firstButtonXpos = WINDOWw/2 - buttonWidth - buttonDistance/2
 firstButtonYpos = WINDOWh/3
 
 version = "Version 0.01"
+gamename = "The RanD0m RuN"
 
 
 class Button(pygame.Surface):
-    def __init__(self,width,height,xpos,ypos):
+    def __init__(self,width,height,xpos,ypos, number_in_menu):
         super().__init__((width,height))
         self.width = width
         self.height = height
         self.xpos = xpos
         self.ypos = ypos
+        self.number = number_in_menu
 
-        self.display_surf = None
+        #self.display_surf = None
         self.image_surf = buttonImage.convert()
         self.image_surf = pygame.transform.scale(self.image_surf,(self.width,self.height))
 
-        self.sound = pygame.mixer.Sound('/home/pi/Git/The_Random_Run/GtaVocals/GameOver.wav')
+        self.sound = pygame.mixer.Sound('/home/pi/Git/The_Random_Run/GtaVocals/Laugh8.wav')
         
     def clicked(self): # Was tun wenn Button geclickt
         self.sound.play()
-
-    
-
+        # Evtl. kurz verzögern
+        return self.number # Nächster Menübildschirm
         
 
-class Startmenu:
-    WINDOWh = WINDOWh
-    WINDOWw = WINDOWw
-
+class Main:
     def __init__(self):
+        pygame.init()
         self.running = True
-        self.display_surf = None
-        self.image_surf = None
-        self.buttons = []
+        self.display_surf = pygame.display.set_mode((WINDOWw,WINDOWh))    # ,pygame.FULLSCREEN  für fullscreen (ehem. None)
+        self.image_surf = pygame.image.load("startmenu.jpg").convert()# (ehem. None)
+        self.image_surf = pygame.transform.scale(self.image_surf,(WINDOWh,WINDOWw))
+        
+        self.menus = [Menu(gamename),Menu("Singleplayer"),Menu("Multiplayer"),
+                          Menu("Highscore"),Menu("Credits"),Menu("Under Construction")] # Test
+        self.menu_in_use = self.menus[0] # Startmenu in Benutzung
+
+        #
+        #
+        #
+        #
 
     def on_execute(self):
-        if self.on_init() == False:
-            self.running = False
 
+        
         pygame.mixer.music.load('tetris.mid')
         pygame.mixer.music.play(-1, 0.0)
 
-        self.buttons.append(Button(buttonWidth,buttonHeight,firstButtonXpos,firstButtonYpos))
-        self.buttons.append(Button(buttonWidth,buttonHeight,firstButtonXpos + buttonWidth + buttonDistance, firstButtonYpos))
-        self.buttons.append(Button(buttonWidth,buttonHeight,firstButtonXpos,firstButtonYpos + buttonHeight + buttonDistance))
-        self.buttons.append(Button(buttonWidth,buttonHeight,firstButtonXpos + buttonWidth + buttonDistance,
-                                                                           firstButtonYpos + buttonHeight + buttonDistance))
+        #buttonding
         
-
         while(self.running):
             for event in pygame.event.get():
                 self.on_event(event)
             #self.on_loop()
 
             self.on_render()
-            #singleButton
         self.endIt()
 
-    def on_init(self):
-        pygame.init()
-        self.display_surf = pygame.display.set_mode((self.WINDOWw,self.WINDOWh))    # ,pygame.FULLSCREEN  für fullscreen
-        self.running = True
-        self.image_surf = pygame.image.load("startmenu.jpg").convert()
-        self.image_surf = pygame.transform.scale(self.image_surf,(self.WINDOWh,self.WINDOWw))
 
     def on_event(self, event):
         if event.type == QUIT or (event.type == KEYUP and event.key == K_ESCAPE):
@@ -83,26 +78,36 @@ class Startmenu:
             (clickX,clickY) = event.pos
             self.mouseclick(clickX,clickY)
 
-    def on_loop(self):
-        pass
-
     def on_render(self):
         self.display_surf.blit(self.image_surf,(0,0))
-        for button in self.buttons:
+        for button in self.menu_in_use.buttons: # button des Menüs                                      
             self.display_surf.blit(button.image_surf,(button.xpos,button.ypos))
-        self.showText(version,(WINDOWw/2,WINDOWh-50),15)
+
+        title = self.menu_in_use.get_title()
+        self.showText(title, textsize = 64)
+
+        self.showText(version,(WINDOWw/2,WINDOWh-50),15) # versionnummer Text
         pygame.display.flip()
 
+    def on_loop(self):
+        pass
+            
+    #Beende Spiel
     def endIt(self):
         pygame.mixer.music.stop()
         pygame.quit()
 
+
     def mouseclick(self, clickX, clickY ):
-        for button in self.buttons:
-            if ((clickX > button.xpos) & clickX < (button.xpos+buttonWidth)) & ((clickY > button.ypos) & (clickY < button.ypos+buttonHeight)):
-                button.clicked()
+        for button in self.menu_in_use.buttons:
+            if ((clickX > button.xpos) & (clickX < (button.xpos+buttonWidth))) & ((clickY > button.ypos) & (clickY < button.ypos+buttonHeight)):
+                new_menu = button.clicked()
+                print(button.xpos+buttonWidth)
+                print(button.ypos+buttonHeight)
+                self.menu_in_use = self.menus[new_menu]
+                return
                 
-    def showText(self,myString,textpos = (50, 50), textsize = 32, waitingTime = 0):
+    def showText(self,myString,textpos = (WINDOWw/2, 50), textsize = 32, waitingTime = 0):
         thisPrint = pygame.font.Font('freesansbold.ttf', textsize).render(myString,True,(255,255,255))
         thisRect = thisPrint.get_rect()
         thisRect.center = (textpos)
@@ -110,7 +115,46 @@ class Startmenu:
         if(waitingTime != 0):
             pygame.display.update()
             pygame.time.wait(waitingTime)  
+        
+
+class Menu:
+
+    def __init__(self,menuname):
+        
+        self.menuname = menuname
+        self.buttons = []
+
+        self.fill_buttons()
+
+    def fill_buttons(self):
+        if self.menuname == gamename:
+            self.buttons.append(Button(buttonWidth,buttonHeight,firstButtonXpos,firstButtonYpos, 1))
+            self.buttons.append(Button(buttonWidth,buttonHeight,firstButtonXpos + buttonWidth + buttonDistance, firstButtonYpos, 2))
+            self.buttons.append(Button(buttonWidth,buttonHeight,firstButtonXpos,firstButtonYpos + buttonHeight + buttonDistance, 3))
+            self.buttons.append(Button(buttonWidth,buttonHeight,firstButtonXpos + buttonWidth + buttonDistance,
+                                                                               firstButtonYpos + buttonHeight + buttonDistance, 4))
+        elif self.menuname == "Highscore":
+            self.buttons.append(Button(buttonWidth,buttonHeight,firstButtonXpos,firstButtonYpos,5))
+            self.buttons.append(Button(buttonWidth,buttonHeight,firstButtonXpos + buttonWidth + buttonDistance, firstButtonYpos, 0))
+            
+        elif self.menuname == "Multiplayer":
+            self.buttons.append(Button(buttonWidth,buttonHeight,firstButtonXpos,firstButtonYpos,5))
+            
+        else:
+            self.buttons.append(Button(buttonWidth,buttonHeight,firstButtonXpos,firstButtonYpos,5))
+    
+
+
+
+
+    #Nur Startmenu render:
+    def get_title(self):
+        return self.menuname 
+
+
+
+
     
         
-start = Startmenu()
+start = Main()
 start.on_execute()
