d70127cfac2e83e8bf09c26e5175432e092add31
Marius Funk <funkm@cip.ifi.lmu.de>
2016-01-04 20:48:10 +0000

    Git tut wieder komische Sachen

--- Texts.py
+++ Texts.py
@@ -10,7 +10,7 @@ starwars_intro = ["A long time ago, in a galaxy far, far away..",
                   "THE RANDOM RUN",
                   ""]
 normal_intro = ["THE RANDOM RUN",
-                  ""] 
+                  "YOU HAVE TO SAVE THE WORLD!!"] 
 
                   
 intro_texts = [starwars_intro,normal_intro]
